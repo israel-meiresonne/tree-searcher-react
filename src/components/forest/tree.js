1437f09ef9863ec4376b18811896d7149c05d7bd
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-28 12:59:28 +0000

    tree: add doc

--- src/components/forest/tree.js
+++ src/components/forest/tree.js
@@ -1,5 +1,9 @@
 import Node from "./node";
 
+/**
+ * Use a random number as a key to force React to re-render after each search
+ * and so expand the collapsed parents.
+ */
 export default function Tree({ root }) {
   return (
     <div className="wrapper-tree">
