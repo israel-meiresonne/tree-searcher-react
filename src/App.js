44ad19f7ed05e0a42ad518a8db79afa01a58b50b
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-28 11:57:21 +0000

    app: add `App` component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { React } from "react";
+import "./styles.css";
+import ForestSearcher from "./components/forest/forestSearcher";
+
+export default function App() {
+  return <ForestSearcher />;
+}
