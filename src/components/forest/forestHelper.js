7d69b23fb26870b5f8673347bfa4a1379cd8dfe2
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-28 11:55:38 +0000

    forestHelper: add helper

---
+++ src/components/forest/forestHelper.js
@@ -0,0 +1,41 @@
+export function buildTree(treeNodes) {
+  const roots = [];
+  const idToNodes = {};
+  for (node of treeNodes) {
+    idToNodes[node._id] = { ...node };
+    idToNodes[node._id].children = [];
+  }
+  for (id in idToNodes) {
+    parentId = idToNodes[id].parent;
+    if (!parentId) {
+      roots.push(idToNodes[id]);
+      continue;
+    }
+    parentId in idToNodes
+      ? idToNodes[parentId].children.push(idToNodes[id])
+      : null;
+  }
+  return { roots, idToNodes };
+}
+
+export function nodeToRoot(node, idToNodes, pathNodes = {}) {
+  pathNodes[node._id] = node;
+  if (node.parent) {
+    let parent = idToNodes[node.parent];
+    return nodeToRoot(parent, idToNodes, pathNodes);
+  } else {
+    return node;
+  }
+}
+
+export function filterNodes(search, treeNodes, idToNodes) {
+  let pathNodes = {};
+  for (const node of treeNodes) {
+    let name = node.name.toLowerCase();
+    let isIncluded = name.includes(search.toLowerCase());
+    if (isIncluded) {
+      nodeToRoot(node, idToNodes, pathNodes);
+    }
+  }
+  return pathNodes;
+}
