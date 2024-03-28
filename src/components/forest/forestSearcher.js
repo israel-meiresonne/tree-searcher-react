e26920bd0916b97397256d3e856f4f1218b1c264
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-28 11:56:05 +0000

    forestSearcher: add root component

---
+++ src/components/forest/forestSearcher.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Tree from "./tree";
+import { buildTree, filterNodes } from "./forestHelper";
+import { treeNodes } from "../../nodes";
+
+const idToNodes = buildTree(treeNodes).idToNodes;
+const defaultRoots = [];
+
+export default function ForestSearcher() {
+  const [rootsState, setRoots] = useState(defaultRoots);
+  function handleSetRoots(search) {
+    let newRoots = [];
+    if (!search) {
+      newRoots = defaultRoots;
+    } else {
+      const pathNodes = filterNodes(search, treeNodes, idToNodes);
+      const filteredNodes = Object.values(pathNodes);
+      const returneds = buildTree(filteredNodes);
+      newRoots = returneds.roots;
+    }
+    setRoots(newRoots);
+  }
+  let i = 0;
+  return (
+    <div className="App">
+      <div className="container-input">
+        <input
+          onChange={(e) => handleSetRoots(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+      </div>
+      <div className="forest">
+        {rootsState.map((root) => (
+          <Tree root={root} />
+        ))}
+      </div>
+    </div>
+  );
+}
